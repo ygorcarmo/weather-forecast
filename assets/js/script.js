7de6e9dcb6787b094a08d851a171f8024c119adf
Ygor <ygor.carmo@gmail.com>
2021-09-27 18:45:35 +1000

    added necessary code for current weather

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+// buttons Start
+var searchBtn = document.getElementById("submitbtn");
+
+searchBtn.addEventListener('click', goLoco);
+// buttons End
+
+// Text/Elements Start
+
+var selectedCity = document.getElementById("city");
+var todayTemp = document.getElementById("currentTemperature");
+var todayWind = document.getElementById("currentWind");
+var todayHumidity = document.getElementById("currentHumidity");
+var todayUVIndex = document.getElementById("currentUVIndex");
+
+var cityTBS = document.getElementById("inserthere");
+
+// Text/Elements End
+
+// API stuff
+
+var apiKey = "e9338296fd417a53918f0986d22214b3";
+function goLoco(event) {
+    event.preventDefault();
+    forecastData = fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityTBS.value}&appid=${apiKey}&units=metric`)
+    .then(response => response.json())
+    .then((data)=>{
+        console.log(data);
+        weatherData(data);
+    })
+
+};
+function weatherData(data) {
+    var oneCall = fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.city.coord.lat}&lon=${data.city.coord.lon}&appid=${apiKey}&units=metric`)
+    .then(response => response.json())
+    .then((weatherLoco) =>{    
+
+        cityDisplayed = cityTBS.value
+        selectedCity.textContent = cityDisplayed.toUpperCase();
+        todayTemp.textContent= weatherLoco.current.temp + " °C"
+        todayWind.textContent= weatherLoco.current.wind_speed + " KM/H" 
+        todayHumidity.textContent= weatherLoco.current.humidity + " %"
+        todayUVIndex.textContent= weatherLoco.current.uvi
+    })
+}
+
+
